01d5583a466028cc66273d0e71998c196c7695c3
Maxim Temryakov <therevors@gmail.com>
2021-05-09 20:30:09 +0300

    connected html using by webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: 'Vue Sketchpad',
+            template: './assets/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
